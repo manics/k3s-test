e4c98edb0d4e4a2869f2b9f45ee41e1193821cb3
Simon Li <orpheus+devel@gmail.com>
2019-06-16 20:30:26 +0100

    k3s 0.6.0-rc5 helm 2.14.1

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -eu
 
-K3S_VERSION=0.6.0-rc3
+K3S_VERSION=0.6.0-rc5
 K3S_SERVER_FLAGS="--tls-san 192.168.99.100"
 LOCAL_PROVISIONER_VERSION=0.0.9
-HELM_VERSION=3.0.0-alpha.1
+#HELM_VERSION=3.0.0-alpha.1
+HELM_VERSION=2.14.1
 
 if [ -f /etc/centos-release ]; then yum install -y -q policycoreutils-python; fi
 
@@ -21,10 +22,19 @@ kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storagec
 while ! kubectl -n kube-system get svc traefik; do sleep 5; done
 kubectl -n kube-system patch svc traefik --type=json -p '[{"op":"replace","path":"/spec/ports/0/nodePort","value":30080},{"op":"replace","path":"/spec/ports/1/nodePort","value":30443}]'
 
-# Helm
-curl -sSfLO https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz
-tar -zxf helm-v$HELM_VERSION-linux-amd64.tar.gz --strip=1 linux-amd64/helm
-mv helm /usr/local/bin/
+if [ "${HELM_VERSION#3.}" = "$HELM_VERSION" ]; then
+  # Helm 2
+  curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-v$HELM_VERSION-linux-amd64.tar.gz | tar -xz -C /usr/local/bin/ --strip-components 1 linux-amd64/helm
+else
+  # Helm 3
+  curl -sSL https://get.helm.sh/helm-v$HELM_VERSION-linux-amd64.tar.gz | tar -xz -C /usr/local/bin/ --strip-components 1 linux-amd64/helm
+fi
+chmod a+x /usr/local/bin/helm
+
+if [ "${HELM_VERSION#3.}" = "$HELM_VERSION" ]; then
+  # Helm 2
+  helm --kubeconfig /etc/rancher/k3s/k3s.yaml init --service-account tiller
+fi
 
 echo export "TERM=${TERM#screen.}" >> /home/vagrant/.bash_profile
 cat << EOF > /home/vagrant/.inputrc
